06b8d3249d6e4288c8d14da58ac7154f4c68a756
natbibi <58271566+natbibi@users.noreply.github.com>
2021-03-27 13:31:32 +0000

    add all princess' profile pics, quotes and change to uniform password

--- server-postgressql/db/2_seed.sql
+++ server-postgressql/db/2_seed.sql
@@ -1,22 +1,30 @@
 INSERT INTO princesses (username, email, password_digest)
 VALUES
-    ('Snow White', 'snowwhite@disney.com', 'omgpasswow2' ),
-    ('Cinderella', 'cinderella@disney.com', 'omgpasswow1' ),
-    ('Aurora', 'aurora@disney.com', 'omgpasswow0' ),
-    ('Ariel', 'ariel@disney.com', 'omgpasswow9' ),
-    ('Belle', 'belle@disney.com', 'omgpasswow8' ),
-    ('Jasmine', 'jasmine@disney.com', 'omgpasswow7' ),
-    ('Pocahontas', 'pocahontas@disney.com', 'omgpasswow6' ),
-    ('Mulan', 'mulan@disney.com', 'omgpasswow5' ),
-    ('Tiana', 'tiana@disney.com', 'omgpasswow4' ),
-    ('Rapunzel', 'rapunzel@disney.com', 'omgpasswow3' ),
-    ('Merida', 'merida@disney.com', 'omgpasswow2' ),
-    ('Moana', 'moana@disney.com', 'omgpasswow1' );
+    ('Snow White', 'snowwhite@disney.com', 'omgpasswow' ),
+    ('Cinderella', 'cinderella@disney.com', 'omgpasswow' ),
+    ('Aurora', 'aurora@disney.com', 'omgpasswow' ),
+    ('Ariel', 'ariel@disney.com', 'omgpasswow' ),
+    ('Belle', 'belle@disney.com', 'omgpasswow' ),
+    ('Jasmine', 'jasmine@disney.com', 'omgpasswow' ),
+    ('Pocahontas', 'pocahontas@disney.com', 'omgpasswow' ),
+    ('Mulan', 'mulan@disney.com', 'omgpasswow' ),
+    ('Tiana', 'tiana@disney.com', 'omgpasswow' ),
+    ('Rapunzel', 'rapunzel@disney.com', 'omgpasswow' ),
+    ('Merida', 'merida@disney.com', 'omgpasswow' ),
+    ('Moana', 'moana@disney.com', 'omgpasswow' );
 
 
 INSERT INTO posts (user_id, body, profile_pic)
 VALUES
-    (1, 'Someday my prince will come', 'https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-10dy163_26bdf0e8.jpeg?region=0,0,600,600'),
-    (2, 'A Dream is a Wish Your Heart Makes', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4G-0CTf_TckObWh9nwrejV9HUz47fGrgNCQ&usqp=CAU'),
-    (3, 'I wonder...', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNtRC1xBzkCostascXYIy2IjMHGOT0XBjPHhknHwxz6kZYvOvVPovAqP5d8pg0_TBRNWI&usqp=CAU'),
-    (9, 'I am almost there!', '');
+    (1, 'Someday my prince will come.', 'https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-10dy163_26bdf0e8.jpeg?region=0,0,600,600'),
+    (2, 'A dream is a wish your heart makes', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4G-0CTf_TckObWh9nwrejV9HUz47fGrgNCQ&usqp=CAU'),
+    (3, 'They say if you dream a thing more than once, it''s sure to come true.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNtRC1xBzkCostascXYIy2IjMHGOT0XBjPHhknHwxz6kZYvOvVPovAqP5d8pg0_TBRNWI&usqp=CAU'),
+    (4, 'You want thingamabobs? I''ve got twenty.', 'https://pm1.narvii.com/6860/da1618e19b4441838996d38b51537b314d10aea7v2_hq.jpg') ,
+    (5, 'I want adventure in the great, wide somewhere.', 'https://i.insider.com/58caab3e7d1fb21a008b4574?width=1136&format=jpeg'),
+    (6, 'Maybe I don''t want to be a princess anymore.', 'https://lumiere-a.akamaihd.net/v1/images/gallery_princess_jasmine_2_1abea4ca.jpeg?region=0%2C0%2C2181%2C1223'),
+    (7, 'I''d rather die tomorrow than live a hundred years without knowing you.', 'https://avatars.githubusercontent.com/u/2048366?s=460&v=4'),
+    (8, 'I never want to see a naked man again!', 'https://i.pinimg.com/originals/87/60/c1/8760c16021f1ac3fc8776df6cc0b6377.jpg'),
+    (9, 'I''ve climbed the mountain, I''ve crossed the river, and I''m almost there.', 'https://static.wikia.nocookie.net/princessdisney/images/8/8d/Tiana.JPG/revision/latest/top-crop/width/360/height/450?cb=20130427062054'),
+    (10, 'BEST. DAY. EVER!', 'https://static.wikia.nocookie.net/disneyprincess/images/d/db/4e18999de38fc38fbff8eec3a250b317.jpg/revision/latest/scale-to-width-down/340?cb=20181217145043'), 
+    (11, 'I want a spell that will change my fate.', 'https://static.wikia.nocookie.net/disney/images/d/d2/Profile_-_Merida.jpeg/revision/latest?cb=20190314143129'), 
+    (12, 'Okay, first, I''m not a princess, I''m the daughter of the chief.', 'https://i.pinimg.com/originals/b4/ff/bb/b4ffbb766ea581356993bf56a4210f6d.jpg');
