33b623ba974efed9d733d8110bd1fe258dad6abf
natbibi <58271566+natbibi@users.noreply.github.com>
2021-03-29 23:13:01 +0100

    move profile pic links to princess table as it makes more sense for profile pics to follow the datat of users rather than posts

--- server-postgressql/db/1_setup.sql
+++ server-postgressql/db/1_setup.sql
@@ -4,7 +4,8 @@ CREATE TABLE princesses (
     id serial PRIMARY KEY, 
     username varchar(20) NOT NULL UNIQUE,
     email varchar(100) NOT NULL UNIQUE,
-    password_digest varchar(500) NOT NULL
+    password_digest varchar(500) NOT NULL,
+    profile_pic varchar(500)
 );
 
 DROP TABLE IF EXISTS posts;
@@ -12,6 +13,5 @@ DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
     id serial PRIMARY KEY,
     user_id INT NOT NULL,
-    body varchar(200) NOT NULL,
-    profile_pic varchar(500)
+    body varchar(200) NOT NULL
 );
